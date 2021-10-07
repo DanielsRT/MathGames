b266dff37341ab1cf96e840da69acef8fbef383b
DanielsRT <robdaniels2001@gmail.com>
2021-10-06 21:35:08 -0500

    Student class complete

--- Student.java
+++ Student.java
@@ -11,10 +11,27 @@ public class Student {
         absences = new ArrayList<>();
     }
 
+    public Student(int id, List<LocalDate> absences) {
+        this.id = id;
+        this.absences = absences;
+    }
+
     @Override
     public String toString() {
         return id + " " + absences;
     }
+
+    public int getId(){return id;}
+    public List<LocalDate> getAbsences(){return absences;}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAbsences(List<LocalDate> absences) {
+        this.absences = absences;
+    }
+
 
     
 }
